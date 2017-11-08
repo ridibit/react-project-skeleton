b330c7cb311764b67b5d90d131b113cbb9f8d11d
ridibit <ridibit@gmail.com>
2017-11-08 22:56:07 +0300

    html-webpack-pligin & index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rootDir = path.resolve(__dirname, 'src');
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src/app.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'app.bundle.js'
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(rootDir, 'index.html')
+		})
+	]
 };
