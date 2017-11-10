ff394c2a6ac3f13aa79fcffb15aa1fb515c2c31e
ridibit <ridibit@gmail.com>
2017-11-10 16:10:20 +0300

    add react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('test');
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+export default class App extends Component {
+	render() {
+		return <div>Hello!</div>;
+	}
+}
+
+render(<App/>, document.getElementById('root'));
